676b8a22d18b5356c7600265304bcc624ad71cf5
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-29 17:50:03 -0300

    feat: create deliveryman use case

---
+++ src/domain/fastRoute/application/use-cases/account/create-deliveryman.ts
@@ -0,0 +1,43 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { DeliverymansRepository } from '../../repositories/deliverymans-repository'
+import {
+  Deliveryman,
+  Vehicle,
+} from '@/domain/fastRoute/enterprise/entities/deliveryman'
+
+interface CreateDeliverymanRequest {
+  userId: string
+  driveLicense: string
+  vehicle: Vehicle
+}
+
+interface CreateDeliverymanResponse {
+  deliveryman: Deliveryman
+}
+
+export class CreateDeliverymanUseCase {
+  constructor(private deliverymansRepository: DeliverymansRepository) {}
+
+  async execute({
+    userId,
+    driveLicense,
+    vehicle,
+  }: CreateDeliverymanRequest): Promise<CreateDeliverymanResponse> {
+    const deliverymanAlreadyExists =
+      await this.deliverymansRepository.findByUserId(userId)
+
+    if (deliverymanAlreadyExists) {
+      throw new Error('Deliveryman already exists')
+    }
+
+    const deliveryman = Deliveryman.create({
+      userId: new UniqueEntityID(userId),
+      driveLicense,
+      vehicle,
+    })
+
+    await this.deliverymansRepository.create(deliveryman)
+
+    return { deliveryman }
+  }
+}
