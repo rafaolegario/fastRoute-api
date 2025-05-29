e25f6de0f2b409ac22d85a5ca54f985bdd742b33
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-29 17:51:12 -0300

    test: create register deliveryman test

---
+++ src/domain/fastRoute/application/use-cases/account/register-deliveryman.spec.ts
@@ -0,0 +1,50 @@
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repositort'
+import { InMemoryDeliverymansRepository } from 'test/repositories/in-memory-deliveryman-repository'
+import { RegisterDeliverymanUseCase } from './register-deliveryman'
+import { CreateUserUseCase } from './create-user'
+import { CreateDeliverymanUseCase } from './create-deliveryman'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let inMemoryDeliverymanRepository: InMemoryDeliverymansRepository
+let createUsersUseCase: CreateUserUseCase
+let createDeliverymanUseCase: CreateDeliverymanUseCase
+let sut: RegisterDeliverymanUseCase
+
+describe('Register DeliveryMan', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    inMemoryDeliverymanRepository = new InMemoryDeliverymansRepository()
+    createDeliverymanUseCase = new CreateDeliverymanUseCase(
+      inMemoryDeliverymanRepository,
+    )
+    createUsersUseCase = new CreateUserUseCase(inMemoryUsersRepository)
+
+    sut = new RegisterDeliverymanUseCase(
+      createUsersUseCase,
+      createDeliverymanUseCase,
+    )
+  })
+
+  it('Should be able to register an deliveryman', async () => {
+    await sut.execute({
+      name: 'JonhDoe',
+      cpf: '123.456.789-11',
+      email: 'jonhDoe@example.com',
+      password: '123456',
+      phone: '(01) 11111-1111',
+      driveLicense: '12345',
+      vehicle: {
+        licensePlate: 'ABC-1234',
+        type: 'motorcycle',
+        model: 'xx 160 xxxx',
+        brand: 'xxxxx',
+        color: 'black',
+        year: 2022,
+        registrationDocumentUrl: 'https://example.com/docs/vehicle.pdf',
+      },
+    })
+
+    expect(inMemoryDeliverymanRepository.items).toHaveLength(1)
+    expect(inMemoryUsersRepository.items).toHaveLength(1)
+  })
+})
