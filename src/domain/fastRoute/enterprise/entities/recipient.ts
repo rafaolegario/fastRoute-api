e035f26e5f9c8a4f261f14f518d5ec94c6101630
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-29 13:15:26 -0300

    feat: create recipient entity

---
+++ src/domain/fastRoute/enterprise/entities/recipient.ts
@@ -0,0 +1,53 @@
+import { Entity } from "@/core/entities/Entity"
+import { UniqueEntityID } from "@/core/entities/unique-entity-id"
+
+type address = {
+  street: string,
+  number: string, 
+  city: string,
+  state: string,
+  zipCode: string,
+  latitude?: number,
+  longitude?: number,
+  complement?:string
+}
+
+export interface RecipientProps {
+  name: string
+  cpf: string
+  phone?: string
+  email?: string
+  address: address
+  createdAt: Date
+}
+
+export class Recipient extends Entity<RecipientProps>{
+  static create(props: RecipientProps, id: UniqueEntityID){
+    const recipient = new Recipient({
+      ...props
+    }, id)
+
+    return recipient
+  }
+
+  get name(){
+    return this.props.name
+  }
+
+  get phone(){
+    return this.props.phone
+  }
+
+  get cpf(){
+    return this.props.cpf
+  }
+
+  get email(){
+    return this.props.email
+  }
+
+  get createdAt(){
+    return this.props.createdAt
+  }
+
+}
