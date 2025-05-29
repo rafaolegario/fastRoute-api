d70e98f69b431681fed0ee9c1180fa160bf675ae
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-29 17:51:04 -0300

    feat: create register deliveryman use case

---
+++ src/domain/fastRoute/application/use-cases/account/register-deliveryman.ts
@@ -0,0 +1,55 @@
+import { Vehicle } from '@/domain/fastRoute/enterprise/entities/deliveryman'
+import { CreateDeliverymanUseCase } from './create-deliveryman'
+import { CreateUserUseCase } from './create-user'
+
+interface RegisterDeliverymanRequest {
+  name: string
+  cpf: string
+  email: string
+  phone: string
+  password: string
+  driveLicense: string
+  vehicle: Vehicle
+}
+
+interface RegisterDeliverymanResponse {
+  userId: string
+  deliverymanId: string
+}
+
+export class RegisterDeliverymanUseCase {
+  constructor(
+    private createUser: CreateUserUseCase,
+    private createDeliveryman: CreateDeliverymanUseCase,
+  ) {}
+
+  async execute({
+    name,
+    cpf,
+    email,
+    phone,
+    password,
+    driveLicense,
+    vehicle,
+  }: RegisterDeliverymanRequest): Promise<RegisterDeliverymanResponse> {
+    const { user } = await this.createUser.execute({
+      name,
+      cpf,
+      email,
+      phone,
+      password,
+      role: ['DELIVERYMAN'],
+    })
+
+    const { deliveryman } = await this.createDeliveryman.execute({
+      userId: user.id.toString(),
+      driveLicense,
+      vehicle,
+    })
+
+    return {
+      userId: user.id.toString(),
+      deliverymanId: deliveryman.id.toString(),
+    }
+  }
+}
