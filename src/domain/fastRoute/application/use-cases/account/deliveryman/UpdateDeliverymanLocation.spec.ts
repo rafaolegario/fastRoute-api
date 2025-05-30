26a142444126695851f2cfd98f938d1fc641b421
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-30 18:07:23 -0300

    test: create update Deliveryman location test

---
+++ src/domain/fastRoute/application/use-cases/account/deliveryman/UpdateDeliverymanLocation.spec.ts
@@ -0,0 +1,49 @@
+import { InMemoryUsersRepository } from 'test/repositories/in-memory-users-repositort'
+import { InMemoryDeliverymansRepository } from 'test/repositories/in-memory-deliveryman-repository'
+import { MakeUser } from 'test/factories/make-user'
+import { UpdateDeliverymanLocationUseCase } from './UpdateDeliverymanLocation'
+import { MakeDeliveryman } from 'test/factories/make-deliveryman'
+
+let inMemoryUsersRepository: InMemoryUsersRepository
+let inMemoryDeliverymanRepository: InMemoryDeliverymansRepository
+let sut: UpdateDeliverymanLocationUseCase
+
+describe('Update DeliveryMan location', () => {
+  beforeEach(() => {
+    inMemoryUsersRepository = new InMemoryUsersRepository()
+    inMemoryDeliverymanRepository = new InMemoryDeliverymansRepository()
+    sut = new UpdateDeliverymanLocationUseCase(inMemoryDeliverymanRepository)
+  })
+
+  it('Should be able to update deliveryman location', async () => {
+    const user = MakeUser({
+      role: ['DELIVERYMAN'],
+    })
+
+    inMemoryUsersRepository.items.push(user)
+
+    const deliveryman = MakeDeliveryman({ userId: user.id })
+
+    inMemoryDeliverymanRepository.items.push(deliveryman)
+
+    const latitude = -23.55052
+    const longitude = -46.633308
+
+    const result = await sut.execute({
+      deliverymanId: deliveryman.id.toString(),
+      currentLocation: {
+        latitude,
+        longitude,
+      },
+    })
+
+    const updatedDeliveryman =
+      inMemoryDeliverymanRepository.items[0].getLocation()
+
+    expect(result.isRight()).toBe(true)
+    expect(updatedDeliveryman).toEqual({
+      latitude,
+      longitude,
+    })
+  })
+})
