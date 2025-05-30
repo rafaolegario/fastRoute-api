3ce0b2cf7143f55ca6ca0c93aa223bf45db341d5
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-30 10:17:22 -0300

    feat: implement save method

--- test/repositories/in-memory-deliveryman-repository.ts
+++ test/repositories/in-memory-deliveryman-repository.ts
@@ -8,6 +8,14 @@ export class InMemoryDeliverymansRepository implements DeliverymansRepository {
     this.items.push(deliveryman)
   }
 
+  async save(deliveryMan: Deliveryman) {
+    const ItemIndex = this.items.findIndex(
+      (item) => item.id.toString() === deliveryMan.id.toString(),
+    )
+
+    this.items[ItemIndex] = deliveryMan
+  }
+
   async findById(Id: string) {
     const DeliveryMan = this.items.find(
       (deliveryMan) => deliveryMan.id.toString() === Id,
