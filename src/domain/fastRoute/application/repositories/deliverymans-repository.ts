bc9e899a49605d04dbbbceeaf33fa38336dfd353
Rafael Olegario <128192379+rafaolegario@users.noreply.github.com>
2025-05-30 10:17:10 -0300

    feat: add new method save

--- src/domain/fastRoute/application/repositories/deliverymans-repository.ts
+++ src/domain/fastRoute/application/repositories/deliverymans-repository.ts
@@ -2,6 +2,7 @@ import { Deliveryman } from '../../enterprise/entities/deliveryman'
 
 export abstract class DeliverymansRepository {
   abstract create(deliveryman: Deliveryman): Promise<void>
+  abstract save(deliveryman: Deliveryman): Promise<void>
   abstract findByUserId(userId: string): Promise<Deliveryman | null>
   abstract findById(id: string): Promise<Deliveryman | null>
 }
